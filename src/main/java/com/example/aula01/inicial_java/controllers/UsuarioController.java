eb372a05e6bee0403d827f47bc324193f7e45a2f
SUELITON DE OLIVEIRA (X01142591190) <x01142591190@tcu.gov.br>
2024-09-25 20:47:19 -0300

    inicio do projeto

---
+++ src/main/java/com/example/aula01/inicial_java/controllers/UsuarioController.java
@@ -0,0 +1,27 @@
+package com.example.aula01.inicial_java.controllers;
+
+
+import com.example.aula01.inicial_java.model.Usuario;
+import com.example.aula01.inicial_java.services.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> buscarUsuarios() {
+        return ResponseEntity.ok(usuarioService.buscarTodosUsuarios());
+    }
+
+}
---
+++ src/main/java/com/example/aula01/inicial_java/services/UsuarioService.java
@@ -0,0 +1,51 @@
+package com.example.aula01.inicial_java.services;
+
+import com.example.aula01.inicial_java.model.Usuario;
+import com.example.aula01.inicial_java.repository.UsuarioRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+
+    public List<Usuario> buscarTodosUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+
+    @PostConstruct
+    public void salvarUsuario() {
+
+        List<Usuario> listaUsuario = new ArrayList<>();
+
+        Usuario usuario1 = Usuario
+                .builder()
+                .cpf("15947275031")
+                .nome("Usuario1")
+                .email("usuario1@gmail.com.br")
+                .build();
+
+        Usuario usuario2 = Usuario
+                .builder()
+                .cpf("15947275031")
+                .nome("Usuario2")
+                .email("usuario2@gmail.com.br")
+                .build();
+
+        listaUsuario.add(usuario1);
+        listaUsuario.add(usuario2);
+
+
+        usuarioRepository.saveAll(listaUsuario);
+    }
+
+
+}
